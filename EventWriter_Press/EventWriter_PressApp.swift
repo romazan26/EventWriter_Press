6c80fd43c603d555cc7235a18b7c8ae68587d8d7
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-21 22:08:40 +0300

    Initial Commit

---
+++ EventWriter_Press/EventWriter_PressApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventWriter_PressApp.swift
+//  EventWriter_Press
+//
+//  Created by Роман on 21.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventWriter_PressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
